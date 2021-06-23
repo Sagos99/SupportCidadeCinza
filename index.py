ba160576ce6d100421cbee9e5460686b67f8a0d5
Sagos <sagoswins@gmail.com>
2021-06-23 09:14:41 -0300

    Agora o bot se conecta ao banco (mas ainda não funciona)

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from discord import channel
 from discord.ext import commands
 from datetime import datetime
 import asyncio
@@ -156,30 +155,23 @@ async def on_reaction_remove(reaction, user):
     print(f'{user.display_name} removeu a reação {reaction.emoji} de uma mensagem\n')
 
 
+# client.run(get_token())
 
-# @tasks.loop(seconds=5.0, count=5)
-# async def send_message():
-#     channel = client.get_channel(850269583066529823)
-#     print('caiu 1')
-#     await channel.send('Quero café! ☕')
+user_id = input(f'Digite seu ID: ')
 
-
-
-#################
+try:
+    user = Helpers.db_consult(None, f'SELECT id, whitelisted, banned FROM vrp_users WHERE id={user_id};')[0]
+except Exception:
+    user = None
 
 
-# @tasks.loop(seconds=2)
-# async def slow_count():
-#     print(slow_count.current_loop)
-
-# @slow_count.after_loop
-# async def after_slow_count():
-#     print('done!')
-
-# slow_count.start()
-
-
-
-# send_message()
-
-client.run(get_token())
+if not user:
+    print('Esse ID não existe')
+elif user[2] == 1:
+    print('Você foi banido da cidade')
+else:
+    if user[1] == 1:
+        print('Seu ID já foi liberado')
+    else:
+        teste = Helpers.db_update(1, f'UPDATE vrp_users SET whitelisted=1 WHERE id={user[0]};')
+        print('Seu ID foi liberado')
--- lib/Helpers.py
+++ lib/Helpers.py
@@ -1,3 +1,8 @@
+import mysql.connector
+
+from lib import config
+
+
 class Helpers():
     def fix_date(self, date):
         if date < 10:
@@ -6,3 +11,39 @@ class Helpers():
             new_format = str(date)
         
         return new_format
+
+    def db_consult(self, consult):
+        if consult:
+            db = mysql.connector.connect(
+                host = config.host,
+                user = config.user,
+                password = config.password,
+                database = config.database
+            )
+
+            cursor = db.cursor()
+            sql = consult
+            cursor.execute(sql)
+            db_data = sorted(cursor.fetchall())
+            db.close()
+        else:
+            db_data = None
+
+        return db_data
+
+    def db_update(self, update):
+        if update:
+            db = mysql.connector.connect(
+                host = config.host,
+                user = config.user,
+                password = config.password,
+                database = config.database
+            )
+
+            cursor = db.cursor()
+            sql = update
+            cursor.execute(sql)
+            db.commit()
+            db.close()
+
+        return None
